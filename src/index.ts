5bddf13c2c127c04263cfdcaf079d6ef0dfe4430
B3none <ablackham2000@gmail.com>
2023-09-10 02:30:14 +0100

    initial project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { chromium } from "playwright";
+
+const browser = await chromium.launch({ headless: true });
+
+const page = await browser.newPage();
+
+await page.goto('https://github.com/b3none/bun-playwright-test');
+
+await browser.close();
